99ae17a15716942e8751c697c966037e0d2ec21d
Rick van Rein <rick@openfortress.nl>
2019-05-16 14:38:37 +0200

    Attempt to package for ManyLinux1 with Travis CI  - Load a ManyLinux1 environment from quai.io  - Run "pip wheel" on many Python versions  - Binary libraries are all very old / compatible  - Scrubbed results are delivered, ready for PyPi  - See https://github.com/pypa/python-manylinux-demo  - Splitting setup.py may be necessary  - Removed library installs (not needed)  - Python3 tripped over a comma!  - Changed the demo name to my installable  - Changed location of final "ls" command  - Listing /io/wheelhouse/  - More explicit about output directory  - More absolute paths to /io/wheelhouse/  - Only the final command was failing, made inquiries  - Listing just what matters  - Whoa... we cannot output our build results?!?

--- subsetup.py
+++ subsetup.py
@@ -8,65 +8,6 @@ from os import path
 here = path.dirname (path.realpath (__file__))
 
 
-#
-# Packaging Instructions -- arpa2shell.cmd, .cmdparser, .amqp
-#
-readme = open (path.join (here, 'README.MD')).read ()
-setuptools.setup (
-
-	# What?
-	name = 'arpa2shell',
-	version = '0.0.0',
-	url = 'https://github.com/arpa2/arpa2shell',
-	description = 'The ARPA2 Shell Collection',
-	long_description = readme,
-	long_description_content_type = 'text/markdown',
-
-	# Who?
-	author = 'Rick van Rein (for the ARPA2 Project)',
-	author_email = 'rick@openfortress.nl',
-
-	# Where?
-	namespace_packages = [ 'arpa2shell', ],
-	packages = [
-		'arpa2shell',
-		'arpa2shell.cmdshell',
-		'arpa2shell.cmdparser',
-		'arpa2shell.amqp',
-	],
-	package_dir = {
-		'arpa2shell'           : path.join (here, 'src'),
-		'arpa2shell.cmdshell'  : path.join (here, 'src', 'cmdshell'),
-		'arpa2shell.cmdparser' : path.join (here, 'src', 'cmdparser'),
-		'arpa2shell.amqp'      : path.join (here, 'src', 'amqp'),
-	},
-
-	# How?
-	entry_points = {
-		'arpa2shell.cmdshell.subclasses' : [
-			'arpa2shell=arpa2shell.cmdshell.meta:Cmd',
-		],
-		'console_scripts' : [
-			'arpa2shell=arpa2shell.cmdshell.meta:main',
-			'arpa2client=arpa2shell.amqp.client:main [JSON]',
-			'arpa2server=arpa2shell.amqp.server:main [JSON]',
-		],
-	},
-
-	# Requirements
-	install_requires = [ 'enum34', 'six', 'decorator' ],
-	extras_require = {
-		'JSON' : [ 'gssapi', 'python-qpid-proton' ],
-	},
-
-)
-
-
-#TODO# For now, just build the main shell
-#TODO# Travis CI builds through "pip whell" which breaks otherwise
-import sys
-sys.exit (0)
-
 
 #
 # Additional setup -- for the arpa2dns shell
